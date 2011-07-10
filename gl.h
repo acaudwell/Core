6ce3a72e5824a2d2341d10289fe5273fdbc63afb
Andrew Caudwell <acaudwell@gmail.com>
2011-07-10 21:57:15 +1200

    Added a macro to check GL errors.

--- gl.h
+++ gl.h
@@ -6,5 +6,17 @@
 #define NO_SDL_GLEXT
 #include "SDL.h"
 #include "SDL_opengl.h"
-
 #endif
+
+#define PRINT_GL_ERRORS
+
+#ifdef PRINT_GL_ERRORS
+#define glCheckError() { \
+    int gl_error = glGetError(); \
+    if(gl_error != GL_NO_ERROR) fprintf(stderr, "GL error %s at %s:%d\n", gluErrorString(gl_error), __FILE__, __LINE__); \
+}
+#else
+#define glCheckError()
+#endif
+
+
