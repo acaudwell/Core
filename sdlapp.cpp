547c8cdc00e8f5f645ce655fec75fdfafa1388d0
Andrew Caudwell <acaudwell@gmail.com>
2012-04-09 22:14:46 +1200

    Don't attach console unless the TERM is msys.
    Check if streams already attached to something.

--- sdlapp.cpp
+++ sdlapp.cpp
@@ -44,12 +44,27 @@ HWND gSDLAppConsoleWindow = 0;
 bool using_parent_console = false;
 
 bool SDLAppAttachToConsole() {
-    if(using_parent_console || gSDLAppConsoleWindow != 0) return using_parent_console;
+    if(using_parent_console)      return true;
+    if(gSDLAppConsoleWindow != 0) return false;
+
+    // if TERM is set to msys, try and attach to console
+    // could possibly add other supported TERMs here if there are any
+
+    char* term = getenv("TERM");
+    if(!term || strcmp(term, "msys")!=0) return false;
 
     if(AttachConsole(ATTACH_PARENT_PROCESS)) {
-        freopen("conin$", "r", stdin);
-        freopen("conout$","w", stdout);
-        freopen("conout$","w", stderr);
+
+        // send stdout to console unless already redirected
+        if (_fileno(stdout) == -1 || _get_osfhandle(fileno(stdout)) == -1) {
+            freopen("conout$","w", stdout);
+        }
+
+        // send stderr to console unless already redirected
+        if (_fileno(stderr) == -1 || _get_osfhandle(fileno(stderr)) == -1) {
+            freopen("conout$","w", stderr);
+        }
+
         using_parent_console = true;
     }
 
